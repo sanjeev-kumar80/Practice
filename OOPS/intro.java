7855c60e7a62a275293799c26fc052a6ac753fd0
Sanjeev Kumar <sanjeev20565@gmail.com>
2026-08-02 02:24:26 +0530

    constructor added

--- OOPS/intro.java
+++ OOPS/intro.java
@@ -3,11 +3,20 @@ package OOPS;
 public class intro {
   public static void main(String[] args) {
     
-    Student sam=new Student();
+    Student sam=new Student(13,23.5f,"Sam");
+
+    Student random=new Student(sam);
+    sam.fun();
 
     System.out.println(sam.name);
     System.out.println(sam.rno);
     System.out.println(sam.marks);
+
+
+    // other (random student) print here
+    System.out.println(random.name);
+    System.out.println(random.rno);
+    System.out.println(random.marks);
   }
 }
 
@@ -18,10 +27,29 @@ class Student{
 
   String name;
 
+    void fun(){
+      System.out.println("hello" + name);
+    }
+
     Student() {
       rno=13;
       marks=20.5f;
       name="Sam Rajput";
     }
+
+     Student(int rno, float marks, String name) {
+        this.rno = rno;
+        this.marks = marks;
+        this.name = name;
+    }
+
+    //one constructor pass into second constructor
+    Student (Student other){
+      this.name=other.name;
+      this.rno=other.rno;
+      this.marks=other.marks;
+    }
+
+    
 
 }
