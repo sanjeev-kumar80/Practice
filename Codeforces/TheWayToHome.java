777c9ee771799e689254880c1372268919bdc9a5
Sanjeev Kumar <sanjeev20565@gmail.com>
2026-09-23 14:28:15 +0530

    question solution

--- Codeforces/TheWayToHome.java
+++ Codeforces/TheWayToHome.java
@@ -1,6 +1,6 @@
 package Codeforces;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class TheWayToHome {
   static int d;
@@ -11,14 +11,44 @@ public class TheWayToHome {
     int n = sc.nextInt();
     d = sc.nextInt();
     String s = sc.next();
-    int ans = solve(0, s);
+    int[] dp = new int[s.length() + 1];
+    Arrays.fill(dp, -1);
 
+    int ans = solvedp(0, s, dp);
     if (ans == Integer.MAX_VALUE) {
       System.out.println(-1);
     } else {
       System.out.println(ans);
     }
+    // int ans = solve(0, s);
 
+    // if (ans == Integer.MAX_VALUE) {
+    // System.out.println(-1);
+    // } else {
+    // System.out.println(ans);
+    // }
+
+  }
+
+  public static int solvedp(int idx, String str, int[] dp) {
+    if (idx == str.length() - 1) {
+      return 0;
+    }
+    if (dp[idx] != -1) {
+      return dp[idx];
+    }
+    int ans = Integer.MAX_VALUE;
+
+    for (int i = idx + 1; i <= Math.min(idx + d, str.length() - 1); i++) {
+      if (str.charAt(i) == '1') {
+        int result = solvedp(i, str, dp);
+
+        if (result != Integer.MAX_VALUE) {
+          ans = Math.min(ans, 1 + result);
+        }
+      }
+    }
+    return dp[idx] = ans;
   }
 
   public static int solve(int idx, String str) {
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 codeforces -->
 dp 910 A  the way to home
+961B
+279--lc
+62 --lc
+174 lc
+
